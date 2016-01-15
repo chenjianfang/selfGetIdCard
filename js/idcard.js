b476c98e6e24d7066475e509ac562075dfe996c2
chenjef <1737752975@qq.com>
2016-01-15 09:05:31 +0800

    '1-15'

--- js/idcard.js
+++ js/idcard.js
@@ -13,28 +13,30 @@ function Init(){
 }
 Init.prototype.clickEvent = function(){
 	var that = this;
+	var $cardnumber = $(".cardnumber");
 	$(".numberCard").click(function(e){
 		that.clickGetValue(e);
 	});
 	$(".delete").click(function(){ //删除 pop()
 		that.valueArry.pop();
-		$(".cardnumber").val(that.valueArry.join(""));
+		$cardnumber.val(that.valueArry.join(""));
 	});
 	$(".clear").click(function(){
 		that.valueArry=[];
-		$(".cardnumber").val(that.valueArry.join(""));
+		$cardnumber.val(that.valueArry.join(""));
 	});
 	$("#post").click(function(){
-		$(".cardnumber").focus();
+		$cardnumber.focus();
 	});
 	/*返回主页*/
 	$(".gotoHome").click(function(){
 		self.location.href="home.html";
 	});
 	$(".cardbutton").click(function(){  //流程开始
-		var carderValue = $(".cardnumber").val();
+		var carderValue = $cardnumber.val();
 		var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
 		var carder = carderValue.match(reg);
+		/*如果身份证信息输入正确*/
 		if(carder){
 			$("#idNumberGet").hide();
 			$(".gotoHome").hide();
@@ -46,18 +48,25 @@ Init.prototype.clickEvent = function(){
 			},1000);
 			var checkfail = setTimeout(function(){
 				$(".checkPage p").html("身份信息检测未通过！请到前台办理！");
-				$(".tipImg-a").hide();
 				$(".tipImg-b").hide();
 				$(".tipImg-c").show();
 				$(".gotoHome").show();
-			},2000)
+			},3000);
+			var faceMessage = setTimeout(function(){
+				$(".checkPage").hide();
+				$(".face-message").show();
+			},5000);
+			var photoId = setTimeout(function(){
+				$(".face-message").hide();
+				$(".photoId").show();
+			},7000);
 			/*下面写接口 上面的只是模拟*/
 			var houtai = false; //接口，如果判断正确，立即执行if里面的
 			if(houtai){
 				clearTimeout(checksucc);
 			};
-			
-		}else{
+
+		}else{  /*身份信息输入不正确*/
 			alert('不通过');
 		}
 	});
@@ -70,6 +79,7 @@ Init.prototype.clickGetValue = function(e){
 		alert("长度过长");
 	}
 }	
+/*设置时间倒计时*/
 Init.prototype.timeRun = function(){
 	var that = this;
 	$(".time .count").html(this.timecount);
